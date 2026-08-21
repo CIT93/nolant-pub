f91bb29a20ac5265be51efe12075a94682a93134
brbcoffee3 <nolantrupp@gmail.com>
2026-08-20 19:13:57 -0700

    Step 2.4 Complete

--- w2/app.js
+++ w2/app.js
@@ -1,6 +1,6 @@
 console.log('Hello from app.js! Your JavaScript is connected and running!');
 const messageDisplayElement = document.getElementById("total-display");
-const updateButton = document.getElementById("add-item-btn");
+const addItemButton = document.getElementById("add-item-btn");
 
 let totalCost = 0;
 const itemPrice = 15
@@ -26,6 +26,5 @@ const handleButtonClick = function(num) {
 
 document.addEventListener('DOMContentLoaded', function(){
     console.log('DOM fully loaded and parsed, App is ready for interaction');
-    updateButton.addEventListener('click', handleButtonClick);
-    messageDisplayElement.textContent = `Welcome, ${userName}! Click the button below to start counting.`
+    addItemButton.addEventListener('click', handleButtonClick);
 });
