47e6dcc2f1c327ae4c08d45298e9d2401637ae6f
brbcoffee3 <nolantrupp@gmail.com>
2026-09-03 23:04:40 -0700

    Step 3 Complete

--- w4/app.js
+++ w4/app.js
@@ -1,19 +1,32 @@
 import * as orderHandler from "./order-handler.js";
+import * as priceCalculator from "./price-calculator.js";
 
 //reference form
 const orderForm = document.getElementById('order-form');
 //reference order summary
 const orderSummary = document.getElementById('order-summary');
 
+const orders = [];
+
 //handle submit
 const handleOrderSubmit = function(event){
     event.preventDefault();
     const formData = orderHandler.getOrderInputs();
+    const total = priceCalculator.calculateTotal(formData);
     orderSummary.textContent = `Ordered ${formData.quantity} ${formData.size} T-Shirts!`;
     if (formData.isWrapped){
         orderSummary.textContent += ` They will be gift wrapped. :)`;
     }
+    const newOrder = {
+    ...formData,
+    total,
+    time: new Date().toISOString()
+    }
+    orders.push(newOrder);
+    console.log(orders);
 }
+
+
 
 const init = function(){
     console.log('DOM initialized, ready to go!');
--- w4/price-calculator.js
+++ w4/price-calculator.js
@@ -4,10 +4,10 @@ const wrapPrice = 2;
 export const calculateTotal = function(orderData){
     let totalPrice = 0;
     totalPrice = orderData.quantity * 15;
-    if(orderData.isWrapped == 'true'){
+    if(orderData.isWrapped == true){
         totalPrice += 2;
     }
     return{
-        wrapPrice: totalPrice
+        price: totalPrice
     }
 }
