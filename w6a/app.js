35c8b89fc00bc4cf27a5826ca05d17e9ef57867d
brbcoffee3 <nolantrupp@gmail.com>
2026-09-14 20:13:54 -0700

    Step 1 Complete

---
+++ w6a/app.js
@@ -0,0 +1,38 @@
+import * as orderHandler from "./order-handler.js";
+import * as priceCalculator from "./price-calculator.js";
+import * as priceSummary from "./results-display.js";
+
+//reference form
+const orderForm = document.getElementById('order-form');
+//reference order summary
+const orderSummary = document.getElementById('order-summary');
+
+const orders = [];
+
+//handle submit
+const handleOrderSubmit = function(event){
+    event.preventDefault();
+    const formData = orderHandler.getOrderInputs();
+    const total = priceCalculator.calculateTotal(formData);
+    //orderSummary.textContent = `Ordered ${formData.quantity} ${formData.size} T-Shirts!`;
+    /*if (formData.isWrapped){
+        orderSummary.textContent += ` They will be gift wrapped. :)`;
+    }*/
+   priceSummary.displaySummary(formData, total);
+    const newOrder = {
+    ...formData,
+    total,
+    time: new Date().toISOString()
+    }
+    orders.push(newOrder);
+    console.log(orders);
+}
+
+
+
+const init = function(){
+    console.log('DOM initialized, ready to go!');
+    orderForm.addEventListener('submit', handleOrderSubmit);
+}
+
+document.addEventListener('DOMContentLoaded', init);
