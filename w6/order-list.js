0abad6e27f6b32d4d7fc5425477d1391dacdb09a
brbcoffee3 <nolantrupp@gmail.com>
2026-09-21 18:49:37 -0700

    Step 2 Complete

---
+++ w6/order-list.js
@@ -0,0 +1,17 @@
+const table = document.getElementById('order-table-body')
+export const renderOrders = function(orders) {
+    table.innerHTML = '';
+    if (orders.length === 0){
+        return;
+    }
+    for(const order of orders){
+        const row = document.createElement('tr');
+        row.dataset.id = order.id;
+        row.innerHTML = `
+        <td>${new Date().toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'})}</td>
+        <td>${order.quantity}</td>
+        <td>${order.size}</td>
+        <td>${order.total}</td>`
+        table.appendChild(row);
+    }
+}
